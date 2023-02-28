2ce0453cd12f110c522f23ffd8b060420c24a2cd
DeepakUser <deepakmegtech@gmail.com>
2023-02-28 19:46:33 +0530

    completed sign in and sign our

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,12 +2,17 @@ const User = require('../models/user');
 
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
-
+        title:"User Profile"
     });
 }
 
 //Rendering Sign In page
 module.exports.signIn = function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: "Codeial | Sign In"
     });
@@ -15,6 +20,11 @@ module.exports.signIn = function(req,res){
 
 //Rendering Sign Up page
 module.exports.signUp = function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    
     return res.render('user_sign_up',{
         title: "Codeial | Sign Up"
     });
@@ -41,4 +51,18 @@ module.exports.create =function(req,res){
             return res.redirect('back');
         }
     })
+}
+
+//sign in and create session for the user
+module.exports.crateSession = function(req, res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res,next){
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+    });
+    return res.redirect('/');
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const express= require('express');
 const router= express.Router();
+const passport = require('passport');
 
 const userController=require('../controllers/users_controller');
 const postController=require('../controllers/post_controller');
-router.get('/profile',userController.profile);
+router.get('/profile', passport.checkAuthentication, userController.profile);
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
 
 router.post('/create',userController.create);
-router.get('/post',postController.post);
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
+),userController.crateSession);
+
+
+// router.get('/post',postController.post);
+router.get('/sign-out',userController.destroySession);
 module.exports=router;
