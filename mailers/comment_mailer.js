7759210ebb27e84757657934e9b0ac63e498305a
DeepakUser <deepakmegtech@gmail.com>
2023-03-28 08:55:00 +0530

    ignored a config mailer file

--- mailers/comment_mailer.js
+++ mailers/comment_mailer.js
@@ -4,12 +4,13 @@ const nodeMailer = require('../config/nodemailer');
 // this is another way of exporting a method
 exports.newComment = (comment)=>{
     // console.log('inside comment mailer');
+    let htmlString  = nodeMailer.rendertemplate({comment:comment},'comments/comment_mailer.ejs');
 
     nodeMailer.transporter.sendMail({
         from:'deepakmegtech.com',
         to: comment.user.email,
         subject: 'New Comment Published',
-        html: '<h1>Yup, your comment is now published</h1>'
+        html: htmlString
     }, (err,info)=>{
         if(err){
             console.log('Error in sending mail', err);
